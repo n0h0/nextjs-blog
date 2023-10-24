b31d4ee1d023c4ed4dc2b4584926989674b547b3
n0h0 <kusakatsubasa@gmail.com>
2023-10-24 20:38:51 +0900

    Dynamic Routes - Polishing the Post Page
    https://nextjs.org/learn/basics/dynamic-routes/polishing-post-page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,8 @@
+import Head from 'next/head';
 import Layout from "../../components/layout";
+import Date from "../../components/date";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import utilStyles from '../../styles/utils.module.css';
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -21,12 +24,15 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date}/>
+        </div>
+      </article>
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
